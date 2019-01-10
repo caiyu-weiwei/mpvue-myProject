809d3d802fefd23f1992bf1f057b3dc6aeb2d93c
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-01-10 19:53:37 +0800

    项目配置文件

---
+++ src/config.js
@@ -0,0 +1,14 @@
+/**
+ * 配置
+ */
+// 生产
+const host = 'https://www.xxx.com'
+const appid = ''
+const appkey = ''
+const config = {
+  host,
+  appid,
+  appkey
+}
+
+export default config
---
+++ src/http/config.js
@@ -0,0 +1,12 @@
+/**
+ * fly 配置文件
+ */
+
+/**
+* 引入fly
+*/
+// npm引入方式
+var Fly = require('flyio/dist/npm/wx')
+// 创建fly实例
+var fly = new Fly()
+
