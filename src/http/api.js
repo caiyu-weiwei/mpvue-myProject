420045905a7b354ad5b548a23d8ceba3dca8a3b2
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-01-11 10:21:57 +0800

    通用请求体

---
+++ src/http/api.js
@@ -0,0 +1,28 @@
+import fly from './config'
+import qs from 'qs'
+import config from '@/config'
+const hostTest = config.hostTest
+const appKey = config.appKey
+const appid = config.appid
+
+/**
+ * 用法
+ * 在页面引入 post
+ * import { post } from '@/http/api'
+ * post(params).then(res => {console.log(res)})
+ */
+
+// 通用 post 请求
+export const post = (params) => {
+  return fly.post(`${hostTest}${params.url}`, qs.stringify(params.data))
+}
+
+// 通用get请求
+export const get = (params) => {
+  return fly.get(`${hostTest}${params.url}`, qs.stringify(params.data))
+}
+
+// 登录请求
+export const login = (params) => {
+  return fly.post(`${hostTest}/login`, params)
+}
