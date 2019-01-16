07debf5ad8df4426069435a613d904cfacccc0f5
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-01-16 10:19:18 +0800

    add plugins createPersistedState

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
 import mutations from './mutations'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
   state,
-  mutations
+  mutations,
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => wx.getStorageSync(key),
+        setItem: (key, value) => wx.setStorageSync(key, value),
+        removeItem: key => {}
+      }
+    })
+  ]
 })
 export default store
